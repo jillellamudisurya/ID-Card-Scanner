e97d0c461eedd4368646821bce933960428319f3
SURYA <S160617@rguktsklm.ac.in>
2021-05-14 11:40:30 +0530

    Sample Data Added

--- lib/OutingStats.dart
+++ lib/OutingStats.dart
@@ -15,31 +15,101 @@ class _OutingStatsState extends State<OutingStats> {
         centerTitle: true,
         elevation: 0.0,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                colors: [Colors.black, Colors.grey, Colors.black])),
-       constraints: BoxConstraints.expand(),
-      
+      body: Scrollbar(
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  colors: [Colors.black, Colors.grey, Colors.black])),
+          constraints: BoxConstraints.expand(),
           child: DataTable(
+            showBottomBorder: true,
             columns: [
-              DataColumn(label: Text('S.No',
-              style: TextStyle(color: Colors.white ),)),
-              DataColumn(label: Text('ID',
-              style: TextStyle(color: Colors.white ),)),
-              DataColumn(label: Text('Name',
-              style: TextStyle(color: Colors.white ),)),
-              DataColumn(label: Text('Phone',
-              style: TextStyle(color: Colors.white ),)),
+              DataColumn(
+                  label: Text(
+                'S.No',
+                style: TextStyle(color: Colors.white),
+              )),
+              DataColumn(
+                  label: Text(
+                'ID',
+                style: TextStyle(color: Colors.white),
+              )),
+              DataColumn(
+                  label: Text(
+                'Name',
+                style: TextStyle(color: Colors.white),
+              )),
+              DataColumn(
+                  label: Text(
+                'Phone',
+                style: TextStyle(color: Colors.white),
+              )),
               //DataColumn(label: Text('Parent Phone')),
               //DataColumn(label: Text('In Time')),
               //DataColumn(label: Text('Out Time')),
             ],
-            rows: [],
+            rows: [
+              DataRow(cells: [
+                DataCell(Text('1',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+                DataCell(Text('S160617',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+                DataCell(Text('J. Surya',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+                DataCell(Text('123456',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('2',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+                DataCell(Text('S160841',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+                DataCell(Text('M. Yashwanth',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+                DataCell(Text('123456',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('3',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+                DataCell(Text('S160755',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+                DataCell(Text('I. Arvindh',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+                DataCell(Text('123456',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('4',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+                DataCell(Text('S160105',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+                DataCell(Text('K. Sai Ganesh',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+                DataCell(Text('123456',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold))),
+              ])
+            ],
           ),
-        
         ),
+      ),
     );
   }
 }
