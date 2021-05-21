90e1395a416c5179b75050f6aed598a50f8c62d7
aravindhippili <aravindhippili@gmail.com>
2021-05-21 17:54:08 +0530

    navigator fixed

--- lib/barcode_result.dart
+++ lib/barcode_result.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
+import 'security_success.dart';
 
 // ignore: must_be_immutable
 class BarcodeResult extends StatelessWidget {
-
-  String result='';
+  String result = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-              title: Text('ID Card Scanner'),
-              backgroundColor: Colors.grey[900],
-              centerTitle: true,
-              elevation: 0.0,
-            ),
+        title: Text('ID Card Scanner'),
+        backgroundColor: Colors.grey[900],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
       body: Container(
         decoration: BoxDecoration(
             gradient: LinearGradient(
@@ -20,50 +20,47 @@ class BarcodeResult extends StatelessWidget {
                 colors: [Colors.black, Colors.grey, Colors.black])),
         child: Column(
           children: [
-            SizedBox(height:70),
-          Center(
-            child: Text(
-              'SUCCESSFUL',
-              style: TextStyle(
-                color: Colors.green,
-                fontSize: 50.0,
-                fontWeight: FontWeight.bold,
+            SizedBox(height: 70),
+            Center(
+              child: Text(
+                'SUCCESSFUL',
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              ),
-
-          ),
-          SizedBox(height:30.0),
-
-          Center(
-            child: Text(
+            ),
+            SizedBox(height: 30.0),
+            Center(
+                child: Text(
               'The Result of SCAN DATA is:: ',
               style: TextStyle(
                 fontSize: 25.0,
                 color: Colors.white,
               ),
-              )
-            ),
-          SizedBox(height:30.0),
-          Center(
-            child: Text(result,
-            style: TextStyle(
-              color: Colors.yellow,
-              fontSize: 30.0,
-              )
-            )
-            ),
-            SizedBox(height:30.0),
+            )),
+            SizedBox(height: 30.0),
+            Center(
+                child: Text(result,
+                    style: TextStyle(
+                      color: Colors.yellow,
+                      fontSize: 30.0,
+                    ))),
+            SizedBox(height: 30.0),
             Center(
               child: ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.pop(context);
-                      
-                    },
-                    icon: Icon(Icons.home),
-                    label: Text('HOME'),
-                  ),
-                )
-        ],
+                onPressed: () {
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => Secsuccess()));
+                },
+                icon: Icon(Icons.home),
+                label: Text('HOME'),
+              ),
+            )
+          ],
         ),
       ),
     );
