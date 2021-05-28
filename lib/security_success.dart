22035809c156f00c4f86a5c814928b31142cd8ff
SURYA <S160617@rguktsklm.ac.in>
2021-05-28 10:43:12 +0530

    secandadminsuccessupdated

--- lib/security_success.dart
+++ lib/security_success.dart
@@ -13,10 +13,9 @@ class _SecsuccessState extends State<Secsuccess> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: Colors.grey[850],
       appBar: AppBar(
         title: Text('ID Card Scanner'),
-        backgroundColor: Colors.grey[900],
+        backgroundColor: Color(0xff34456e),
         centerTitle: true,
         elevation: 0.0,
         actions: <Widget>[
@@ -40,55 +39,108 @@ class _SecsuccessState extends State<Secsuccess> {
       ),
       drawer: SecSideNav(),
       body: Container(
+        padding: EdgeInsets.only(left: 20, right: 20),
         decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                colors: [Colors.black, Colors.grey, Colors.black])),
+          image: DecorationImage(
+              image: AssetImage("assets/BG.png"), fit: BoxFit.cover),
+        ),
         child: Column(
           children: [
-            SizedBox(height: 30),
-            Center(
-              child: Text(
-                'SECURITY',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30.0,
-                  color: Colors.white,
-                  letterSpacing: 2.0,
-                ),
-              ),
-            ),
-            SizedBox(height: 30),
+            SizedBox(height: 150),
             Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   CircleAvatar(
-                    backgroundImage: AssetImage('assets/image.jpg'),
+                    backgroundImage: AssetImage('assets/adminsecurity.jpg'),
                     radius: 50.0,
                   ),
                   SizedBox(height: 30),
-                  ElevatedButton.icon(
+                  TextButton(
                     onPressed: () {
                       Navigator.push(context,
                           MaterialPageRoute(builder: (context) => Outing()));
                     },
-                    icon: Icon(Icons.call_missed_outgoing),
-                    label: Text('Outing'),
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.all(0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                    ),
+                    child: Ink(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                        gradient: LinearGradient(
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                          colors: [
+                            Color(0xff6673b4),
+                            Color(0xff5867a2),
+                            Color(0xff3f507f),
+                            Color(0xff34456e)
+                          ],
+                        ),
+                      ),
+                      child: Container(
+                        alignment: Alignment.center,
+                        constraints: BoxConstraints(
+                            maxWidth: double.infinity, minHeight: 50),
+                        child: Text(
+                          "OUTING",
+                          style: TextStyle(
+                              letterSpacing: 2,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
                   ),
                   SizedBox(height: 30),
                   CircleAvatar(
-                    backgroundImage: AssetImage('assets/image.jpg'),
+                    backgroundImage: AssetImage('assets/adminsecurity.jpg'),
                     radius: 50.0,
                   ),
                   SizedBox(height: 30),
-                  ElevatedButton.icon(
+                  TextButton(
                     onPressed: () {
                       Navigator.push(context,
                           MaterialPageRoute(builder: (context) => Leave()));
                     },
-                    icon: Icon(Icons.time_to_leave),
-                    label: Text('Leave'),
+                    style: TextButton.styleFrom(
+                      padding: EdgeInsets.all(0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                    ),
+                    child: Ink(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                        gradient: LinearGradient(
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight,
+                          colors: [
+                            Color(0xff6673b4),
+                            Color(0xff5867a2),
+                            Color(0xff3f507f),
+                            Color(0xff34456e)
+                          ],
+                        ),
+                      ),
+                      child: Container(
+                        alignment: Alignment.center,
+                        constraints: BoxConstraints(
+                            maxWidth: double.infinity, minHeight: 50),
+                        child: Text(
+                          "LEAVE",
+                          style: TextStyle(
+                              letterSpacing: 2,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
