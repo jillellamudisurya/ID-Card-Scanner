89287f8dfc799b66a9c8bce74084000ac8d260ea
aravindhippili <aravindhippili@gmail.com>
2021-05-30 16:55:30 +0530

    added date picker

---
+++ lib/outing/select_date.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+import 'package:idcardscanner/outing/OutingStats.dart';
+
+class SelectDate extends StatefulWidget {
+  @override
+  _SelectDateState createState() => _SelectDateState();
+}
+
+class _SelectDateState extends State<SelectDate> {
+  String _selectedDate = '';
+  _getDateFromDate(DateTime dt) {
+    var date = dt;
+    return date.day.toString().padLeft(2, '0') +
+        "-" +
+        date.month.toString().padLeft(2, "0") +
+        "-" +
+        date.year.toString();
+  }
+
+  void _onSelectionChanged(DateRangePickerSelectionChangedArgs args) {
+    setState(() {
+      _selectedDate = _getDateFromDate(args.value);
+    });
+  }
+
+  void initState() {
+    super.initState();
+    _selectedDate = _getDateFromDate(DateTime.now());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xff34456e),
+          title: Text('Date Picker'),
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        body: Padding(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Text(
+                  'Selected Date: ' + _selectedDate,
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 400,
+                child: SfDateRangePicker(
+                  headerHeight: 30,
+                  headerStyle:
+                      DateRangePickerHeaderStyle(textAlign: TextAlign.center),
+                  onSelectionChanged: _onSelectionChanged,
+                  selectionMode: DateRangePickerSelectionMode.single,
+                  initialSelectedDate: DateTime.now(),
+                  minDate: DateTime(2021, 05, 29, 0, 0, 0),
+                  maxDate: DateTime.now(),
+                  showNavigationArrow: true,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              OutingStats(date: _selectedDate)));
+                },
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.all(0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6),
+                  ),
+                ),
+                child: Ink(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
+                    gradient: LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [
+                        Color(0xff6673b4),
+                        Color(0xff5867a2),
+                        Color(0xff3f507f),
+                        Color(0xff34456e)
+                      ],
+                    ),
+                  ),
+                  child: Container(
+                    alignment: Alignment.center,
+                    constraints: BoxConstraints(
+                        maxWidth: double.infinity, minHeight: 50),
+                    child: Text(
+                      "NEXT",
+                      style: TextStyle(
+                          letterSpacing: 2,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
