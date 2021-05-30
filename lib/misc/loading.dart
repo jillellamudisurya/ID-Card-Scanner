beba19a45383119717ad06aad1ec587e500dbe96
aravindhippili <aravindhippili@gmail.com>
2021-05-30 10:23:46 +0530

    loading color changed

--- lib/misc/loading.dart
+++ lib/misc/loading.dart
@@ -5,7 +5,7 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blueAccent,
+      color: Color(0xff34456e),
       child: Center(
         child: SpinKitWave(
           color: Colors.white,
